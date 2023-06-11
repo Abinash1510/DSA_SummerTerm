a1775c9d0d065a6bc5b97cc328f7c07f8724528d
Abinash1510 <patra011991@gmail.com>
2023-06-12 03:38:57 +0530

    added gitignore

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+  cout<<"PRONOU";
+}
+
+int main(){
+   system("cls");
+   
+   solve();
+   
+   return 0;
+}
