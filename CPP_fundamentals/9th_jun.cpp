711696c486efd645fb05fb9c0889091735c1ab4c
Abinash1510 <patra011991@gmail.com>
2023-06-12 03:16:48 +0530

    trying it

--- CPP_fundamentals/9th_jun.cpp
+++ CPP_fundamentals/9th_jun.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     for (int i = 1; i <= 3; i++) {
         for (int j = 1; j <= 3; j++) {
-            cout << i << " " << j << endl;
+            cout << i << " " << j;
         }
     }
     return 0;
