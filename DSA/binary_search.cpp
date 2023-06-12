1fec94ed271aef45f135d52f497c3b758f762ac6
Abinash1510 <patra011991@gmail.com>
2023-06-13 01:48:58 +0530

    padhai

--- DSA/binary_search.cpp
+++ DSA/binary_search.cpp
@@ -3,8 +3,33 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
+int binary_search(vector<int>v, int key){
+   int n = v.size();
+   sort(v.begin(), v.end());
+   int left = 0; int right = n-1;
+   while(left<=right){
+      int mid = (left + right)/2;
+      if(v[mid]==key){
+         return mid;
+      }
+      else if(v[mid]<key){
+         left = mid+1;
+      }
+      else{
+         right = mid-1;
+      }
+   }
+   return -1;
+}
+
+
 void solve(){
-  
+   int n; cin>>n;
+   vector<int>v(n);
+   for(auto &i : v) cin>>i;
+   int key; cin>>key;
+
+   cout<<binary_search(v, key);
 }
 
 int main(){
