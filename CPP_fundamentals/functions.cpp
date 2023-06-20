3787a754820184c205783188e19e4898c28e5ea7
Abinash1510 <patra011991@gmail.com>
2023-06-21 02:59:48 +0530

    hehe

---
+++ CPP_fundamentals/functions.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+
+//returnType functionName(dataType parameterName){
+
+//}
+
+void myFunc(){
+
+
+}
+
+
+int main(){
+
+}
---
+++ DSA/arrays.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+// #define 
+
+int main(){
+    // int x = 5;
+    // int arr[5] = {1,2,3,4,5};
+
+    // cout<<arr[2]<<endl;
+
+    // for(auto &i:arr) cout<<i<<" ";
+    
+
+
+
+    int n;
+    cin>>n;
+    // int arr[n];
+    int *arr= new int(n);
+    
+
+}
